18b1be26ed5dc67f554ffea05e98adbb24358dc1
Jonas Gonka <jonas.gonka@mni.thm.de>
2018-02-21 15:11:31 +0100

    updated the script

--- scripts/ddduc.js
+++ scripts/ddduc.js
@@ -1,8 +1,16 @@
-$(function () {
+function horrorDoki() {
+    var timeout = getRandomInt(3000, 10000);
     setTimeout(function () {
         $(".img").attr("src", "sources/images/oops2.png");
-    }, 4000);
+    }, timeout);
     setTimeout(function () {
         $(".img").attr("src", "sources/images/Casual_Monika.jpg");
-    }, 4050);
+        horrorDoki();
+    }, timeout + getRandomInt(50, 150));
+}
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+$(function () {
+    horrorDoki();
 });
