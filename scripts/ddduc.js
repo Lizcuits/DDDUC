a3db4ba53059585389217817996151de2a584664
Jonas Gonka <jonas.gonka@mni.thm.de>
2018-02-21 14:45:43 +0100

    Set up the website and run a simple .js test

---
+++ scripts/ddduc.js
@@ -0,0 +1,8 @@
+$(function () {
+    setTimeout(function () {
+        $(".img").attr("src", "sources/images/oops2.png");
+    }, 4000);
+    setTimeout(function () {
+        $(".img").attr("src", "sources/images/Casual_Monika.jpg");
+    }, 4030);
+});
